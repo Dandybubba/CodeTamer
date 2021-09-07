1758c7aecc4e0291815b201a007ff8dc17cbc4d9
Delandis Massengill <landismassengill@gmail.com>
2021-09-07 17:46:47 -0500

    this is for my studies

---
+++ testMyself.js
@@ -0,0 +1,21 @@
+let message;
+message = 'Imma Study Hard';
+console.log(message);
+
+for (let car = 0; car < ['mercedes', 'jeep', 'pontiac'].length; car++) {
+    const element = ['mercedes', 'jeep', 'pontiac'][car];
+    
+}
+var mycar = { make: 'Honda', model: 'Accord', year: 1998 };
+var x, y;
+
+x = mycar.make; // x gets the value "Honda"
+
+myFunc(mycar);
+y = mycar.make;
+
+buttonA.onclick = function () {
+    let name = prompt('What is your name?');
+    alert('Hello ' + name + ', nice to see you!');
+    headingA.textContent = 'Welcome ' + name;
+  };
